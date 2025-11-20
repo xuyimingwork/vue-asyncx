0c816b8cf675e7d403119cebca9e939b6572441c
xuyimingwork <xuyimingwork@foxmail.com>
2025-11-20 11:19:07 +0800

    test: add test for tracker

--- src/__test__/utils.test.ts
+++ src/__test__/utils.test.ts
@@ -4,9 +4,11 @@ import { upperFirst } from "../utils";
 describe('upperFirst', () => {
   test('无值情况', () => {
     expect(upperFirst('')).toBe('')
+    expect(upperFirst(undefined as any)).toBe('')
   })
 
   test('首字母大写', () => {
     expect(upperFirst('abc')).toBe('Abc')
+    expect(upperFirst('Abc')).toBe('Abc')
   })
 })
--- src/__test__/utils.tracker.test.ts
+++ src/__test__/utils.tracker.test.ts
@@ -1,103 +1,266 @@
 import { describe, expect, test } from 'vitest'
-import { createFunctionTracker } from '../utils'
+import { createFunctionTracker, Track, Tracker } from '../utils'
+
+function check(t: Track, expired: { call: boolean, progress: boolean, result: boolean, ok: boolean }) {
+  const MAP = { 
+    call: undefined, 
+    progress: 'progress',
+    result: 'result',
+    ok: 'result:ok'
+  }
+  // result:ok 过期，result 一定过期
+  if (t.expired('result:ok')) expect(t.expired('result')).toBe(true)
+  Object.keys(expired).forEach((key) => {
+    const state = key as unknown as keyof typeof expired
+    expect(t.expired(MAP[state as keyof typeof MAP] as any)).toBe((expired[state]))
+  })
+}
+
+function init(count: number): Track[] & { tracker: Tracker }  {
+  const tracker = createFunctionTracker()
+  const tracks: Track[] = []
+  for(let i = 0; i < count; i++) {
+    const track = tracker()  
+    tracks.push(track)
+    const prev = tracks.slice(0, tracks.length - 1)
+    // 新调用发生后，所有之前的调用全部过期
+    if (prev.length) prev.every(track => check(track, { call: true, progress: false, result: false, ok: false }))
+    check(track, { call: false, progress: false, result: false, ok: false })
+  }
+  return Object.assign(tracks, { tracker })
+}
 
 describe('createFunctionTracker', () => {
-  test('normal tracker', () => {
-    const tracker = createFunctionTracker()
+  test('1 tracker', () => {
+    const [t1] = init(1)
 
-    const t1 = tracker()
-    // initially not expired
-    expect(t1.expired()).toBe(false)
-    expect(t1.expired('progress')).toBe(false)
-    expect(t1.expired('result')).toBe(false)
+    t1.finish()
+    check(t1, { call: false, progress: true, result: false, ok: false })
+  })
 
-    // mark progress for t1, should still not be expired
+  test('1 tracker - progress', () => {
+    const [t1] = init(1)
+
     t1.progress()
-    expect(t1.expired()).toBe(false)
-    expect(t1.expired('progress')).toBe(false)
-    expect(t1.expired('result')).toBe(false)
+    check(t1, { call: false, progress: false, result: false, ok: false })
 
-    // mark finish for t1, progress should be expired
     t1.finish()
-    expect(t1.expired()).toBe(false)
-    expect(t1.expired('progress')).toBe(true)
-    expect(t1.expired('result')).toBe(false)
+    check(t1, { call: false, progress: true, result: false, ok: false })
   })
 
-  test('2 trackers, later progress & finish first', () => {
-    const tracker = createFunctionTracker()
+  test('1 tracker - error', () => {
+    const [t1] = init(1)
 
-    const t1 = tracker()
-    expect(t1.expired()).toBe(false)
-    const t2 = tracker()
-
-    // initially not expired
-    expect(t1.expired()).toBe(true)
-    expect(t1.expired('progress')).toBe(false)
-    expect(t1.expired('result')).toBe(false)
-    expect(t2.expired()).toBe(false)
-    expect(t2.expired('progress')).toBe(false)
-    expect(t2.expired('result')).toBe(false)
-
-    // mark progress for t2, t1 should be expired, t2 should not
-    t2.progress()
-    expect(t1.expired()).toBe(true)
-    expect(t1.expired('progress')).toBe(true)
-    expect(t1.expired('result')).toBe(true)
-    expect(t2.expired()).toBe(false)
-    expect(t2.expired('progress')).toBe(false)
-    expect(t2.expired('result')).toBe(false)
-
-    // mark finish for t2, progress should be expired
-    t2.finish()
-    expect(t1.expired()).toBe(true)
-    expect(t1.expired('progress')).toBe(true)
-    expect(t1.expired('result')).toBe(true)
-    expect(t2.expired()).toBe(false)
-    expect(t2.expired('progress')).toBe(true)
-    expect(t2.expired('result')).toBe(false)
+    t1.finish(true)
+    check(t1, { call: false, progress: true, result: false, ok: false })
   })
 
-  test('2 trackers, random order', () => {
-    const tracker = createFunctionTracker()
+  test('2 trackers - 12', () => {
+    const [t1, t2] = init(2)
 
-    const t1 = tracker()
-    const t2 = tracker()
-
-    // initially not expired
-    expect(t1.expired()).toBe(true)
-    expect(t1.expired('progress')).toBe(false)
-    expect(t1.expired('result')).toBe(false)
-    expect(t2.expired()).toBe(false)
-    expect(t2.expired('progress')).toBe(false)
-    expect(t2.expired('result')).toBe(false)
-
-    // mark progress for t2, t1 should be expired, t2 should not
-    t2.progress()
-    expect(t1.expired()).toBe(true)
-    expect(t1.expired('progress')).toBe(true)
-    expect(t1.expired('result')).toBe(true)
-    expect(t2.expired()).toBe(false)
-    expect(t2.expired('progress')).toBe(false)
-    expect(t2.expired('result')).toBe(false)
-
-    // mark finish for t1, same with above
     t1.finish()
-    expect(t1.expired()).toBe(true)
-    expect(t1.expired('progress')).toBe(true)
-    expect(t1.expired('result')).toBe(true)
-    expect(t2.expired()).toBe(false)
-    expect(t2.expired('progress')).toBe(false)
-    expect(t2.expired('result')).toBe(false)
-
-    // mark finish for t2, progress should be expired
+    check(t1, { call: true, progress: true, result: false, ok: false })
+    check(t2, { call: false, progress: false, result: false, ok: false })
     t2.finish()
-    expect(t1.expired()).toBe(true)
-    expect(t1.expired('progress')).toBe(true)
-    expect(t1.expired('result')).toBe(true)
-    expect(t2.expired()).toBe(false)
-    expect(t2.expired('progress')).toBe(true)
-    expect(t2.expired('result')).toBe(false)
+    check(t1, { call: true, progress: true, result: true, ok: true })
+    check(t2, { call: false, progress: true, result: false, ok: false })
   })
 
+  test('2 trackers - 12 progress 12', () => {
+    const [t1, t2] = init(2)
+
+    t1.progress()
+    check(t1, { call: true, progress: false, result: false, ok: false })
+    check(t2, { call: false, progress: false, result: false, ok: false })
+    t2.progress()
+    check(t1, { call: true, progress: true, result: true, ok: true })
+    check(t2, { call: false, progress: false, result: false, ok: false })
+
+    t1.finish()
+    check(t1, { call: true, progress: true, result: true, ok: true })
+    check(t2, { call: false, progress: false, result: false, ok: false })
+    t2.finish()
+    check(t1, { call: true, progress: true, result: true, ok: true })
+    check(t2, { call: false, progress: true, result: false, ok: false })
+  })
+
+  test('2 trackers - 12 error 12', () => {
+    const [t1, t2] = init(2)
+
+    t1.finish(true)
+    check(t1, { call: true, progress: true, result: false, ok: false })
+    check(t2, { call: false, progress: false, result: false, ok: false })
+    t2.finish(true)
+    // ok data come from previous call, as long as no later update/ok, ok keep false
+    check(t1, { call: true, progress: true, result: true, ok: false })
+    check(t2, { call: false, progress: true, result: false, ok: false })
+  })
+
+  test('2 trackers - 12 error 1', () => {
+    const [t1, t2] = init(2)
+
+    t1.finish(true)
+    check(t1, { call: true, progress: true, result: false, ok: false })
+    check(t2, { call: false, progress: false, result: false, ok: false })
+    t2.finish()
+    check(t1, { call: true, progress: true, result: true, ok: true })
+    check(t2, { call: false, progress: true, result: false, ok: false })
+  })
+
+  test('2 trackers - 12 error 2', () => {
+    const [t1, t2] = init(2)
+
+    t1.finish()
+    check(t1, { call: true, progress: true, result: false, ok: false })
+    check(t2, { call: false, progress: false, result: false, ok: false })
+    t2.finish(true)
+    check(t1, { call: true, progress: true, result: true, ok: false })
+    check(t2, { call: false, progress: true, result: false, ok: false })
+  })
+
+  test('2 trackers - 21', () => {
+    const [t1, t2] = init(2)
+
+    t2.finish()
+    check(t1, { call: true, progress: true, result: true, ok: true })
+    check(t2, { call: false, progress: true, result: false, ok: false })
+    t1.finish()    
+    check(t1, { call: true, progress: true, result: true, ok: true })
+    check(t2, { call: false, progress: true, result: false, ok: false })
+  })
+
+  test('2 trackers - 21 error 21', () => {
+    const [t1, t2] = init(2)
+
+    t2.finish(true)
+    check(t1, { call: true, progress: true, result: true, ok: false })
+    check(t2, { call: false, progress: true, result: false, ok: false })
+    t1.finish(true)    
+    check(t1, { call: true, progress: true, result: true, ok: false })
+    check(t2, { call: false, progress: true, result: false, ok: false })
+  })
+
+  test('2 trackers - 21 error 2', () => {
+    const [t1, t2] = init(2)
+
+    t2.finish(true)
+    check(t1, { call: true, progress: true, result: true, ok: false })
+    check(t2, { call: false, progress: true, result: false, ok: false })
+    t1.finish()    
+    check(t1, { call: true, progress: true, result: true, ok: false })
+    check(t2, { call: false, progress: true, result: false, ok: false })
+  })
+
+  test('2 trackers - 21 error 1', () => {
+    const [t1, t2] = init(2)
+
+    t2.finish()
+    check(t1, { call: true, progress: true, result: true, ok: true })
+    check(t2, { call: false, progress: true, result: false, ok: false })
+    t1.finish(true)    
+    check(t1, { call: true, progress: true, result: true, ok: true })
+    check(t2, { call: false, progress: true, result: false, ok: false })
+  })
+
+  test('3 trackers - 123', () => {
+    const [t1, t2, t3] = init(3)
+
+    t1.finish()
+    check(t1, { call: true, progress: true, result: false, ok: false })
+    check(t2, { call: true, progress: false, result: false, ok: false })
+    check(t3, { call: false, progress: false, result: false, ok: false })
+    t2.finish()
+    check(t1, { call: true, progress: true, result: true, ok: true })
+    check(t2, { call: true, progress: true, result: false, ok: false })
+    check(t3, { call: false, progress: false, result: false, ok: false })
+    t3.finish()
+    check(t1, { call: true, progress: true, result: true, ok: true })
+    check(t2, { call: true, progress: true, result: true, ok: true })
+    check(t3, { call: false, progress: true, result: false, ok: false })
+  })
+
+  test('3 trackers - 123 error 123', () => {
+    const [t1, t2, t3] = init(3)
+
+    t1.finish(true)
+    check(t1, { call: true, progress: true, result: false, ok: false })
+    check(t2, { call: true, progress: false, result: false, ok: false })
+    check(t3, { call: false, progress: false, result: false, ok: false })
+    t2.finish(true)
+    check(t1, { call: true, progress: true, result: true, ok: false })
+    check(t2, { call: true, progress: true, result: false, ok: false })
+    check(t3, { call: false, progress: false, result: false, ok: false })
+    t3.finish(true)
+    check(t1, { call: true, progress: true, result: true, ok: false })
+    check(t2, { call: true, progress: true, result: true, ok: false })
+    check(t3, { call: false, progress: true, result: false, ok: false })
+  })
+
+  test('3 trackers - 123 error 12', () => {
+    const [t1, t2, t3] = init(3)
+
+    t1.finish(true)
+    check(t1, { call: true, progress: true, result: false, ok: false })
+    check(t2, { call: true, progress: false, result: false, ok: false })
+    check(t3, { call: false, progress: false, result: false, ok: false })
+    t2.finish(true)
+    check(t1, { call: true, progress: true, result: true, ok: false })
+    check(t2, { call: true, progress: true, result: false, ok: false })
+    check(t3, { call: false, progress: false, result: false, ok: false })
+    t3.finish()
+    check(t1, { call: true, progress: true, result: true, ok: true })
+    check(t2, { call: true, progress: true, result: true, ok: true })
+    check(t3, { call: false, progress: true, result: false, ok: false })
+  })
+
+  test('3 trackers - 123 error 13', () => {
+    const [t1, t2, t3] = init(3)
+
+    t1.finish(true)
+    check(t1, { call: true, progress: true, result: false, ok: false })
+    check(t2, { call: true, progress: false, result: false, ok: false })
+    check(t3, { call: false, progress: false, result: false, ok: false })
+    t2.finish()
+    check(t1, { call: true, progress: true, result: true, ok: true })
+    check(t2, { call: true, progress: true, result: false, ok: false })
+    check(t3, { call: false, progress: false, result: false, ok: false })
+    t3.finish(true)
+    check(t1, { call: true, progress: true, result: true, ok: true })
+    check(t2, { call: true, progress: true, result: true, ok: false })
+    check(t3, { call: false, progress: true, result: false, ok: false })
+  })
+
+  test('3 trackers - 123 error 23', () => {
+    const [t1, t2, t3] = init(3)
+
+    t1.finish()
+    check(t1, { call: true, progress: true, result: false, ok: false })
+    check(t2, { call: true, progress: false, result: false, ok: false })
+    check(t3, { call: false, progress: false, result: false, ok: false })
+    t2.finish(true)
+    check(t1, { call: true, progress: true, result: true, ok: false })
+    check(t2, { call: true, progress: true, result: false, ok: false })
+    check(t3, { call: false, progress: false, result: false, ok: false })
+    t3.finish(true)
+    check(t1, { call: true, progress: true, result: true, ok: false })
+    check(t2, { call: true, progress: true, result: true, ok: false })
+    check(t3, { call: false, progress: true, result: false, ok: false })
+  })
+
+  test('3 trackers - 312', () => {
+    const [t1, t2, t3] = init(3)
+
+    t3.finish()
+    check(t1, { call: true, progress: true, result: true, ok: true })
+    check(t2, { call: true, progress: true, result: true, ok: true })
+    check(t3, { call: false, progress: true, result: false, ok: false })
+    t1.finish()
+    check(t1, { call: true, progress: true, result: true, ok: true })
+    check(t2, { call: true, progress: true, result: true, ok: true })
+    check(t3, { call: false, progress: true, result: false, ok: false })
+    t2.finish()
+    check(t1, { call: true, progress: true, result: true, ok: true })
+    check(t2, { call: true, progress: true, result: true, ok: true })
+    check(t3, { call: false, progress: true, result: false, ok: false })
+  })
 })
