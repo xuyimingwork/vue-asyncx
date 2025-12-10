b508c2e0ff78b7416500d5fb2bd0b61c07e1db9f
xuyimingwork <xuyimingwork@foxmail.com>
2025-12-10 11:56:10 +0800

    test: support type test

---
+++ src/__test__/use-async.test-d copy.ts
@@ -0,0 +1,37 @@
+import { describe, expectTypeOf, test } from 'vitest'
+import { useAsync } from '../use-async'
+import { ComputedRef, Ref } from 'vue'
+
+describe('useAsync type', () => {
+  test('should be function', () => {
+    expectTypeOf(useAsync).toBeFunction()
+  })
+  test('should return type ok when missing name', () => {
+    const fn = () => ''
+    type ResultWithoutName<Fn> = {
+      method: Fn,
+      methodLoading: Ref<boolean>,
+      methodArguments: ComputedRef<[]>, 
+      methodArgumentFirst: ComputedRef<undefined>, 
+      methodError: Ref<any>
+    }
+    expectTypeOf(useAsync(fn)).toEqualTypeOf<ResultWithoutName<typeof fn>>()
+    expectTypeOf(useAsync('', fn)).toEqualTypeOf<ResultWithoutName<typeof fn>>()
+    const fnAsync = () => Promise.resolve('')
+    expectTypeOf(useAsync(fnAsync)).toEqualTypeOf<ResultWithoutName<typeof fnAsync>>()
+    expectTypeOf(useAsync('', fnAsync)).toEqualTypeOf<ResultWithoutName<typeof fnAsync>>()
+  })
+  test('should return type ok with name', () => {
+    const fn = () => ''
+    type ResultWithName<Fn> = {
+      one: Fn,
+      oneLoading: Ref<boolean>,
+      oneArguments: ComputedRef<[]>, 
+      oneArgumentFirst: ComputedRef<undefined>, 
+      oneError: Ref<any>
+    }
+    expectTypeOf(useAsync('one', fn)).toEqualTypeOf<ResultWithName<typeof fn>>()
+    const fnAsync = () => Promise.resolve('')
+    expectTypeOf(useAsync('one', fnAsync)).toEqualTypeOf<ResultWithName<typeof fnAsync>>()
+  })
+})
