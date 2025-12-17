443492f487db3d90f61bce1d34f73c38de544b9c
xuyimingwork <xuyimingwork@foxmail.com>
2025-12-17 15:46:14 +0800

    refactor: reorg code

---
+++ src/use-async.types.d.ts
@@ -0,0 +1,25 @@
+import { ComputedRef, Ref, WatchCallback, WatchOptions, WatchSource } from 'vue'
+import type { Simplify, StringDefaultWhenEmpty } from './utils/index'
+
+export type UseAsyncResult<Fn extends (...args: any) => any, Name extends string> = Simplify<{
+  [K in StringDefaultWhenEmpty<Name, 'method'>]: Fn
+} & {
+  [K in `${StringDefaultWhenEmpty<Name, 'method'>}Loading`]: Ref<boolean>
+} & {
+  [K in `${StringDefaultWhenEmpty<Name, 'method'>}Arguments`]: ComputedRef<Parameters<Fn>>
+} & {
+  [K in `${StringDefaultWhenEmpty<Name, 'method'>}ArgumentFirst`]: ComputedRef<Parameters<Fn>['0']>
+} & {
+  [K in `${StringDefaultWhenEmpty<Name, 'method'>}Error`]: Ref<any>
+}>
+
+interface UseAsyncWatchOptions<Fn extends (...args: any) => any> extends WatchOptions {
+  handlerCreator?: (fn: Fn) => WatchCallback
+}
+
+export type UseAsyncOptions<Fn extends (...args: any) => any> = Simplify<{
+  watch?: WatchSource | WatchSource[]
+  watchOptions?: UseAsyncWatchOptions<Fn>
+  immediate?: boolean 
+  setup?: (fn: Fn) => ((...args: any) => any) | void
+}>
