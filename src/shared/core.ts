cb43210b560c5a9ea32c07cbce3727c8518b32e3
xuyimingwork <xuyimingwork@foxmail.com>
2025-12-18 18:13:18 +0800

    refactor: reorg code

---
+++ src/shared/core.ts
@@ -0,0 +1,37 @@
+import { FunctionMonitorWithTracker, Simplify, withFunctionMonitor } from "../utils";
+import { MergeReturnTypes, runFunctions } from "../utils/base";
+import { useSetup } from "./function";
+
+export type StateCreator = (params: { monitor: FunctionMonitorWithTracker }) => any
+export type StateCreatorParams = Parameters<StateCreator>[0]
+export type BoostCreator<Fn> = (params: { method: Fn, monitor: FunctionMonitorWithTracker }) => any
+
+export function useCore<
+  Fn extends (...args: any) => any,
+  Options,
+  const StateCreators extends readonly StateCreator[],
+  const BoostCreators extends readonly BoostCreator<Fn>[]
+>({
+  fn,
+  options,
+  stateCreators,
+  boostCreators,
+}: {
+  fn: Fn,
+  options?: Options,
+  stateCreators: StateCreators, 
+  boostCreators: BoostCreators
+}): MergeReturnTypes<readonly [...StateCreators, ...BoostCreators]> {
+  const { run, monitor } = withFunctionMonitor(fn)
+  const states = runFunctions(stateCreators.map((creator) => () => creator({
+    monitor
+  })))
+  const method = useSetup(run, options)
+  const boosts = runFunctions(boostCreators.map((creator) => () => creator({
+    method, monitor
+  })))
+  return { 
+    ...states, 
+    ...boosts 
+  } as any
+}
--- src/use-async/use-async.ts
+++ src/use-async/use-async.ts
@@ -1,21 +1,70 @@
-import { parseArguments } from "../shared/function"
+import { parseArguments, useWatch } from "../shared/function"
 import type { UseAsyncOptions, UseAsyncResult } from './types'
-import { useAsyncBase } from "../use-async-base/use-async-base"
-
-
+import { BoostCreator, StateCreator, useCore } from "../shared/core"
+import { useStateError, useStateLoading, useStateParameters } from "../shared/state"
+import type { Ref, ComputedRef } from "vue"
+import { StringDefaultWhenEmpty } from "../utils"
 
 export function useAsync<Fn extends (...args: any) => any>(fn: Fn, options?: UseAsyncOptions<Fn>): UseAsyncResult<Fn, 'method'>
 export function useAsync<Fn extends (...args: any) => any, Name extends string = string>(name: Name, fn: Fn, options?: UseAsyncOptions<Fn>): UseAsyncResult<Fn, Name>
 export function useAsync(...args: any[]): any {
   const { name = 'method', fn, options } = parseArguments(args)
-  const { method, loading, parameters, parameterFirst, error } = useAsyncBase(fn, options)
-  return {
-    [name]: method,
-    [`${name}Loading`]: loading,
-    [`${name}Arguments`]: parameters,
-    [`${name}ArgumentFirst`]: parameterFirst,
-    [`${name}Error`]: error,
-  }
+  return _useAsync({ name, fn, options })
 }
-
 export { useAsync as useAsyncFunction }
+
+export function _useAsync<
+  Name extends string,
+  Fn extends (...args: any) => any, 
+  const StateCreators extends readonly StateCreator[] = [],
+  const BoostCreators extends readonly BoostCreator<Fn>[] = []
+>({ 
+  name, fn, options,
+  stateCreators = [] as unknown as StateCreators,
+  boostCreators = [] as unknown as BoostCreators,
+}: { 
+  name: Name, fn: Fn, options?: any,
+  stateCreators?: StateCreators, 
+  boostCreators?: BoostCreators
+}) {
+  type _Name = StringDefaultWhenEmpty<Name, 'method'>
+  return useCore({ 
+    fn, 
+    options,
+    stateCreators: [
+      ({ monitor }): {
+        [K in `${_Name}Loading`]: Ref<boolean>
+      } => ({
+        [`${name}Loading`]: useStateLoading(monitor)
+      } as any),
+      ({ monitor }): {
+        [K in `${_Name}Error`]: Ref<boolean>
+      } => ({
+        [`${name}Error`]: useStateError(monitor)
+      } as any),
+      ({ monitor }): {
+        [K in `${_Name}Arguments`]: ComputedRef<Parameters<Fn>>
+      } & {
+        [K in `${_Name}ArgumentFirst`]: ComputedRef<Parameters<Fn>['0']>
+      } => {
+        const { parameters, parameterFirst } = useStateParameters<Fn>(monitor)
+        return {
+          [`${name}Arguments`]: parameters,
+          [`${name}ArgumentFirst`]: parameterFirst,
+        } as any
+      },
+      ...stateCreators,
+    ] as const,
+    boostCreators: [
+      ({ method }): {
+        [K in _Name]: Fn
+      } => ({
+        [name]: method
+      } as any),
+      ...boostCreators,
+      ({ method }) => {
+        useWatch(method, options)
+      }
+    ] as const
+  })
+}
