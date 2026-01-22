56be6245698b6094d1f6ab39b34a6c516e46d08b
xuyimingwork <xuyimingwork@foxmail.com>
2026-01-22 10:05:29 +0800

    fix: remove withAddonData

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
+import { withAddonGroup } from "@/addons/group"
 import { getAsyncDataContext, unFirstArgumentEnhanced, useAsyncData } from "@/hooks/use-async-data/use-async-data"
 import { useAsync, useAsyncFunction } from "@/hooks/use-async/use-async"
-import { withAddonData } from "@/addons/data"
-import { withAddonGroup } from "@/addons/group"
 
-export { 
-  useAsync, 
-  useAsyncFunction,
-  useAsyncData, 
+export {
   getAsyncDataContext,
-  unFirstArgumentEnhanced,
-  withAddonData,
-  withAddonGroup,
-} 
+  unFirstArgumentEnhanced, useAsync, useAsyncData, useAsyncFunction, withAddonGroup
+}
